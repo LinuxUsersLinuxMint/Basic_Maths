cfff893014178ef6ac3038e35dd5ac31851d9426
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-09-30 14:49:01 +0300

    Add files via upload

---
+++ Basic_Maths.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"Copyright© 2023 LinuxUsersLinuxMint"
+"Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır."
+"Basic_Maths All Rights Reserved under the GPL(General Public License)."
+"Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
+"A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
+
+def addition(x,y):
+    x=x
+    y=y
+    global result
+    result=x+y
+    print("result: {0} + {1} = {2}". format(x,y,result))
+def Extraction(x,y):
+    x=x
+    y=y
+    global result
+    result=x-y
+    print("result: {0} - {1} = {2}". format(x,y,result))
+def Multiplication(x,y):
+    x=x
+    y=y
+    global result
+    result=x*y
+    print("result: {0} * {1} = {2}". format(x,y,result))
+def Division(x,y):
+    x=x
+    y=y
+    global result
+    result=x/y
+    print("result: {0} / {1} = {2}". format(x,y,result))
+def Percentage(x,y):
+    x=x
+    y=y
+    global result
+    result=x%y
+    print("result: {0} % {1} = {2}". format(x,y,result))
