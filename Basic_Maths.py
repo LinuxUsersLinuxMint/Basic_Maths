7f2e5bd9f986d1d42f89327a2881ddac9bc8dd80
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-18 19:13:09 +0300

    Result Dialog variable added. The user can change the result dialog variable.

--- Basic_Maths.py
+++ Basic_Maths.py
@@ -5,56 +5,56 @@
 "Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 "A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 
-def addition(x,y):
+def addition(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x+y
-    print("result: {0} + {1} = {2}". format(x,y,result))
-def Extraction(x,y):
+    print("{3} {0} + {1} = {2}". format(x,y,result,ResultDialog))
+def Extraction(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x-y
-    print("result: {0} - {1} = {2}". format(x,y,result))
-def Multiplication(x,y):
+    print("{3} {0} - {1} = {2}". format(x,y,result,ResultDialog))
+def Multiplication(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x*y
-    print("result: {0} * {1} = {2}". format(x,y,result))
-def Division(x,y):
+    print("{3} {0} * {1} = {2}". format(x,y,result,ResultDialog))
+def Division(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x/y
-    print("result: {0} / {1} = {2}". format(x,y,result))
-def Percentage(x,y):
+    print("{3} {0} / {1} = {2}". format(x,y,result,ResultDialog))
+def Percentage(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x%y
-    print("result: {0} % {1} = {2}". format(x,y,result))
-def FullDivision(x,y):
+    print("{3} {0} % {1} = {2}". format(x,y,result,ResultDialog))
+def FullDivision(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x//y
-    print("result: {0} // {1} = {2}". format(x,y,result))
-def TakingExponents(x,y):
+    print("{3} {0} // {1} = {2}". format(x,y,result,ResultDialog))
+def TakingExponents(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x ** y
-    print("result {0} ** {1} = {2}". format(x,y,result))
-def TakingRoots(x,y):
+    print("{3} {0} ** {1} = {2}". format(x,y,result,ResultDialog))
+def TakingRoots(x,y,ResultDialog):
     x=x
     y=y
     global result
     result=x ** (1/y)
-    print("result: {0} ** (1/{1}) = {2}". format(x,y,result))
+    print("{3} {0} ** (1/{1}) = {2}". format(x,y,result,ResultDialog))
 def SqaureRoot(x):
     x=x
     global result
     result=x ** (1/2)
-    print("result: {0} ** (1/2) = {1}". format(x,result))
+    print("{3} {0} ** (1/2) = {1}". format(x,result,ResultDialog))
--- Example/basic_maths_calc.py
+++ Example/basic_maths_calc.py
@@ -7,18 +7,18 @@ number2=int(input('N2: '))
 select=str(input('Select (Add, Ext, Mul, Div, Per, FullDiv, TakeEx): '))
 
 if select=="Add":
-    addition(number1,number2)
+    addition(number1,number2,"Result:")
 elif select=="Ext":
-    Extraction(number1,number2)
+    Extraction(number1,number2,"Result:")
 elif select=="Mul":
-    Multiplication(number1,number2)
+    Multiplication(number1,number2,"Result:")
 elif select=="Div":
-    Division(number1,number2)
+    Division(number1,number2,"Result:")
 elif select=="Per":
-    Percentage(number1,number2)
+    Percentage(number1,number2,"Result:")
 elif select=="FullDiv":
-    FullDivision(number1,number2)
+    FullDivision(number1,number2,"Result:")
 elif select=="TakeEx":
-    TakingExponents(number1,number2)
+    TakingExponents(number1,number2,"Result:")
 else:
     print("Invalid Process...!")
