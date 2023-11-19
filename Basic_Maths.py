00d3e3c66d2076212b5921a7dd98bb5af4f84aef
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-19 21:15:13 +0300

    Update Basic_Maths.py
    Signed-off-by: Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>

--- Basic_Maths.py
+++ Basic_Maths.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"Copyright© 2023 LinuxUsersLinuxMint"
-"Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır."
-"Basic_Maths All Rights Reserved under the GPL(General Public License)."
-"Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
-"A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
+"""Copyright© 2023 LinuxUsersLinuxMint
+Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
+Basic_Maths All Rights Reserved under the GPL(General Public License).
+Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
+A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"""
 
 def addition(x,y,ResultDialog):
     x=x
