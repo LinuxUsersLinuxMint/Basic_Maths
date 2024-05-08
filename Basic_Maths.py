6d4d0c0d29a9202fbb78561cf86a88ffc6582eef
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-05-08 22:32:53 +0300

    Minor edit.

--- Basic_Maths.py
+++ Basic_Maths.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Copyright© 2023-2024 LinuxUsersLinuxMint
+""" Copyright© 2023-2024 LinuxUsersLinuxMint
 Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 Basic_Maths All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"""
+Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Basic_Maths
+A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/Basic_Maths"""
 
 global result
 
 PYTHON_LIB_NAME="Basic_Maths"
 PYTHON_LIB_LICENCE="GPL2"
-PYTHON_LIB_VER="2.1_installbatnotification"
+PYTHON_LIB_VER="2.2"
 PYTHON_LIB_AUTHOR="LinuxUsersLinuxMint"
 
 # Math functions
