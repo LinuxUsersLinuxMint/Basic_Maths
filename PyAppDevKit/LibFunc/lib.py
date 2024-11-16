ea34f085edb0eafb8a8381e6d0bddc5519b0c8ae
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-11-16 15:52:16 +0000

    Update Basic_Maths.

--- PyAppDevKit/LibFunc/lib.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Copyright© 2023-2024 LinuxUsersLinuxMint
-PyAppDevKit Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
-PyAppDevKit All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/PyAppDevKit"""
-
-import os, time, platform
-from PyAppDevKit.InfoLib.pyappdevkit_info import *
--- PyAppDevKit/LibFunc/pyappdevkit.py
+++ PyAppDevKit/LibFunc/pyappdevkit.py
@@ -5,7 +5,7 @@ PyAppDevKit All Rights Reserved under the GPL(General Public License).
 Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
 A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/PyAppDevKit"""
 
-from PyAppDevKit.LibFunc.lib import *
+import os, time, platform
 
 def error_msg(error_dialog):
     print(error_dialog)
@@ -58,18 +58,31 @@ def all_exit(ExitSelectDialog,userTimeDialog,exitDialog,errormsgDialog):
 def program_welcome_msg(welcome_msg,cfg):
     print(welcome_msg)
     if cfg == 1:
-        LibAbout()
+        #LibAbout()
+        pass
     elif cfg == 0:
         pass
 
-def program_info(programnamedialog,program_name,programversiondialog,program_version,programsupportosdialog,program_support_os,programlicencedialog,program_licence,programimplementedcontractsdialog,program_imp_contracts,programimplementedcontractswebsitedialog,program_imp_contracts_web_site,programauthordialog,program_author,programauthorwebsitedialog,program_author_web_site,programreleasedatedialog,program_rs_date,programlastupdatedatedialog,program_last_update_date):
-    print("{0} {1}". format(programnamedialog,program_name))
-    print("{0} {1}". format(programversiondialog,program_version))
-    print("{0} {1}". format(programsupportosdialog,program_support_os))
-    print("{0} {1}". format(programlicencedialog,program_licence))
-    print("{0} {1}". format(programimplementedcontractsdialog,program_imp_contracts))
-    print("{0} {1}". format(programimplementedcontractswebsitedialog,program_imp_contracts_web_site))
-    print("{0} {1}". format(programauthordialog,program_author))
-    print("{0} {1}". format(programauthorwebsitedialog,program_author_web_site))
-    print("{0} {1}". format(programreleasedatedialog,program_rs_date))
-    print("{0} {1}". format(programlastupdatedatedialog,program_last_update_date))
+def program_info(dialog_one,dialog_one_t,dialog_two,dialog_two_t,dialog_three,dialog_three_t,dialog_four,dialog_four_t,dialog_five,dialog_five_t,dialog_six,dialog_six_t,dialog_seven,dialog_seven_t,dialog_eigth,dialog_eight_t,dialog_nine,dialog_nine_t,dialog_ten,dialog_ten_t):
+    print("{0} {1}". format(dialog_one,dialog_one_t))
+    print("{0} {1}". format(dialog_two,dialog_two_t))
+    print("{0} {1}". format(dialog_three,dialog_three_t))
+    print("{0} {1}". format(dialog_four,dialog_four_t))
+    print("{0} {1}". format(dialog_five,dialog_five_t))
+    print("{0} {1}". format(dialog_six,dialog_six_t))
+    print("{0} {1}". format(dialog_seven,dialog_seven_t))
+    print("{0} {1}". format(dialog_eigth,dialog_eight_t))
+    print("{0} {1}". format(dialog_nine,dialog_nine_t))
+    print("{0} {1}". format(dialog_ten,dialog_ten_t))
+
+def library_info(dialog_one,dialog_one_t,dialog_two,dialog_two_t,dialog_three,dialog_three_t,dialog_four,dialog_four_t,dialog_five,dialog_five_t,dialog_six,dialog_six_t,dialog_seven,dialog_seven_t,dialog_eigth,dialog_eight_t,dialog_nine,dialog_nine_t,dialog_ten,dialog_ten_t):
+    print("{0} {1}". format(dialog_one,dialog_one_t))
+    print("{0} {1}". format(dialog_two,dialog_two_t))
+    print("{0} {1}". format(dialog_three,dialog_three_t))
+    print("{0} {1}". format(dialog_four,dialog_four_t))
+    print("{0} {1}". format(dialog_five,dialog_five_t))
+    print("{0} {1}". format(dialog_six,dialog_six_t))
+    print("{0} {1}". format(dialog_seven,dialog_seven_t))
+    print("{0} {1}". format(dialog_eigth,dialog_eight_t))
+    print("{0} {1}". format(dialog_nine,dialog_nine_t))
+    print("{0} {1}". format(dialog_ten,dialog_ten_t))
