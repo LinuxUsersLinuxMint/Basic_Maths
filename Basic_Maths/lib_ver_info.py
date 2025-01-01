137e47bfe997503890e384e1cdccf5be5d95722c
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-01-01 18:26:42 +0300

    Copyright notice has been updated.

--- Basic_Maths/lib_ver_info.py
+++ Basic_Maths/lib_ver_info.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Copyright© 2023-2024 LinuxUsersLinuxMint
+""" Copyright© 2023-2025 LinuxUsersLinuxMint
 Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 Basic_Maths All Rights Reserved under the GPL(General Public License).
 Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Basic_Maths
--- PyAppDevKit/LibFunc/pyappdevkit.py
+++ PyAppDevKit/LibFunc/pyappdevkit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Copyright© 2023-2024 LinuxUsersLinuxMint
+""" Copyright© 2023-2025 LinuxUsersLinuxMint
 PyAppDevKit Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 PyAppDevKit All Rights Reserved under the GPL(General Public License).
 Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
