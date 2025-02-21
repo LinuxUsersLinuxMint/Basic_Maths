6e08e64bd4cc83f1aa4a6664812507a06e4c67a9
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-21 23:39:50 +0300

    Installation files have been deleted for a feature planned for the near future.

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@title Basic_Maths Install
-
-set /p usrpath=You Python Path: 
-pause
-echo install Basic_Maths
-copy Basic_Maths\Basic_Maths.py %usrpath%
-pause
-exit
--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-sudo su
-
-cp Basic_Maths/Basic_Maths.py /usr/lib/python3.10/
