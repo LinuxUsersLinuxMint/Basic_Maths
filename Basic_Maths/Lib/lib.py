5ea746345e978fc0e9d401b44648ebe928d620a5
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-02-21 23:28:42 +0300

    Comment lines have been edited.

--- Basic_Maths/Lib/lib.py
+++ Basic_Maths/Lib/lib.py
@@ -2,8 +2,8 @@
 """ Copyright© 2023-2025 LinuxUsersLinuxMint
 Basic_Maths Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 Basic_Maths All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Basic_Maths
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/Basic_Maths"""
+Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Basic_Maths
+A Copy of This Software is published on GitHub To view: https://github.com/LinuxUsersLinuxMint/Basic_Maths"""
 
 from math import *
 from PyAppDevKit.LibFunc.pyappdevkit import *
--- PyAppDevKit/LibFunc/pyappdevkit.py
+++ PyAppDevKit/LibFunc/pyappdevkit.py
@@ -2,8 +2,8 @@
 """ Copyright© 2023-2025 LinuxUsersLinuxMint
 PyAppDevKit Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 PyAppDevKit All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/PyAppDevKit"""
+Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/PyAppDevKit
+A Copy of This Software is published on GitHub To view: https://github.com/LinuxUsersLinuxMint/PyAppDevKit"""
 
 def time(number):
     while number > 0:
